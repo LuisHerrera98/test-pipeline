0637e66e9fb28e9dd520bac116dfa3bb8d5c9361
luisherrera98 <luchoherrerafernandez@gmail.com>
2023-08-26 05:25:54 -0300

    test commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 // Middleware para parsear JSON en las peticiones
 app.use(express.json());
